7e5a4131bae4988ae36a609f2bdd97dad7fdd7d2
sherman07 <shermanzheng8@gmail.com>
2024-03-19 12:47:23 +1300

    Initial push

--- AS91896 - Sherman Zheng.py
+++ AS91896 - Sherman Zheng.py
@@ -1,6 +1,8 @@
 import easygui
 
+#The Task Dictionary that stores the information of all tasks.
 Task_Dictionary = {
+    #Task 1
     "T1": {
         "Title": "Design Homepage",
         "Description": "Create a mockup of the homepage",
@@ -8,6 +10,7 @@ Task_Dictionary = {
         "Priority": "3",
         "Status": "In Progress"
     },
+    #Task 2
     "T2": {
         "Title": "Implement Login page",
         "Description": "Create the Login page for the website",
@@ -15,6 +18,7 @@ Task_Dictionary = {
         "Priority": "3",
         "Status": "Blocked"
     },
+    #Task 3
     "T3": {
         "Title": "Fix navigation menu",
         "Description": "Fix the navigation menu to be more user-friendly",
@@ -22,6 +26,7 @@ Task_Dictionary = {
         "Priority": "1",
         "Status": "Not Started"
     },
+    #Task 4
     "T4": {
         "Title": "Add payment processing",
         "Description": "Implement payment processing for website",
@@ -29,6 +34,7 @@ Task_Dictionary = {
         "Priority": "2",
         "Status": "In Progress"
     },
+    #Task 5
     "T5": {
         "Title": "Create an About Us Page",
         "Description": "Create a page with information about the company",
@@ -38,17 +44,21 @@ Task_Dictionary = {
     }
     
 }
+#The Team Member Dictionary that stores the information of all member.
 Team_Member_Dictionary = {
+    #Team member Jone Smith
     "JSM": {
         "Name": "Jone Smith",
         "Email": "John@techvision.com",
         "Task Assigned": "[“T1”,“T2”]"
     },
+    #Team member Jane Love
     "JSM": {
         "Name": "Jane Love",
         "Email": "Jane@techvision.com",
         "Task Assigned": "[“T4”]"
     },
+    #Team member Bob Dillon
     "BDI": {
         "Name": "Bob Dillon",
         "Email": "Bob@techvision.com",
@@ -57,4 +67,5 @@ Team_Member_Dictionary = {
 
 }
 
+easygui.msgbox("Hii")
 easygui.msgbox("Hi")
