a1beb6c2b896fdf69daee0a971094dcfaa501f7b
sherman07 <shermanzheng8@gmail.com>
2024-03-19 12:34:19 +1300

    Initial push

--- AS91896 - Sherman Zheng.py
+++ AS91896 - Sherman Zheng.py
@@ -1,3 +1,60 @@
 import easygui
 
+Task_Dictionary = {
+    "T1": {
+        "Title": "Design Homepage",
+        "Description": "Create a mockup of the homepage",
+        "Assignee": "JSM",
+        "Priority": "3",
+        "Status": "In Progress"
+    },
+    "T2": {
+        "Title": "Implement Login page",
+        "Description": "Create the Login page for the website",
+        "Assignee": "JSM",
+        "Priority": "3",
+        "Status": "Blocked"
+    },
+    "T3": {
+        "Title": "Fix navigation menu",
+        "Description": "Fix the navigation menu to be more user-friendly",
+        "Assignee": "None",
+        "Priority": "1",
+        "Status": "Not Started"
+    },
+    "T4": {
+        "Title": "Add payment processing",
+        "Description": "Implement payment processing for website",
+        "Assignee": "JLO",
+        "Priority": "2",
+        "Status": "In Progress"
+    },
+    "T5": {
+        "Title": "Create an About Us Page",
+        "Description": "Create a page with information about the company",
+        "Assignee": "BDI",
+        "Priority": "1",
+        "Status": "Blocked"
+    }
+    
+}
+Team_Member_Dictionary = {
+    "JSM": {
+        "Name": "Jone Smith",
+        "Email": "John@techvision.com",
+        "Task Assigned": "[“T1”,“T2”]"
+    },
+    "JSM": {
+        "Name": "Jane Love",
+        "Email": "Jane@techvision.com",
+        "Task Assigned": "[“T4”]"
+    },
+    "BDI": {
+        "Name": "Bob Dillon",
+        "Email": "Bob@techvision.com",
+        "Task Assigned": "[“T5”]"
+    }
+
+}
+
 easygui.msgbox("Hi")
